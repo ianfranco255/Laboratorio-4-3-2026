950d86706fdc32d29317de706aa4e9f1ccbb6b9a
ianfranco255 <ianfranco.vallejoet32@gmail.com>
2026-03-16 15:39:40 -0300

    nuevo

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
-﻿int cantAlumnos = 0;
+int cantAlumnos = 0;
 int cantComputadoras = 0;
 Console.WriteLine("Ingrese la cantidad de computadoras: ");
 string? recibido = Console.ReadLine();
-if (int.TryParse(recibido, out int num))
-    Console.WriteLine("Ingrese la cantidad de Alumnos: ");
+Console.WriteLine("Ingrese la cantidad de Alumnos: ");
 Console.ReadLine();
-if (cantAlumnos > cantComputadoras) { Console.WriteLine("Faltan computadoras para la sala de alumnos."); }
+if (int.TryParse(recibido, out int num))
+{
+    cantComputadoras = num;
+}
 else
 {
-    Console.WriteLine("Hay suficientes computadoras en la sala");
+    Console.WriteLine("ERROR");
 }
+if (cantAlumnos > cantComputadoras) 
+{ 
+    Console.WriteLine("Faltan computadoras para la sala de alumnos."); 
+}
+else
+    {
+    Console.WriteLine("Hay suficientes computadoras en la sala");
+    }
