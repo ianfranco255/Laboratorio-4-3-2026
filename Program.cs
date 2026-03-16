339e96e5f8869670239a57effdcdf8aea865d9f1
ianfranco255 <ianfranco.vallejoet32@gmail.com>
2026-03-16 14:00:02 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int cantAlumnos = 0;
+int cantComputadoras = 0;
+Console.WriteLine("Ingrese la cantidad de computadoras: ");
+string? recibido = Console.ReadLine();
+if (int.TryParse(recibido, out int num))
+    Console.WriteLine("Ingrese la cantidad de Alumnos: ");
+Console.ReadLine();
+if (cantAlumnos > cantComputadoras) { Console.WriteLine("Faltan computadoras para la sala de alumnos."); }
+else
+{
+    Console.WriteLine("Hay suficientes computadoras en la sala");
+}
