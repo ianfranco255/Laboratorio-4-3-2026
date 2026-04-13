967f2d7451e45825fab4b65e5e9ac627562ee1ff
ianfranco255 <ianfranco.vallejoet32@gmail.com>
2026-04-13 17:03:47 -0300

    Actividad0

--- Program.cs
+++ Program.cs
@@ -1,22 +1,49 @@
-int cantAlumnos = 0;
-int cantComputadoras = 0;
+int cantComps = 0; //VP
+int cantAlumn = 0; //VP
+string input; // para readline
+bool exitosaConv; // para tryParse
+
+
+
+Console.WriteLine("Ingrese la cantidad de alumnos: ");
+input = Console.ReadLine(); // se ingresa cant alum
+exitosaConv = int.TryParse(input, out int numero); // se verifica si el dato es int | devuelve bool e int | int numero para ingresar a VP
+
+while (exitosaConv == false|| numero < 0)  // <- Mientras conversión exitosa sea falso o numero menor a 0 hacer:
+{
+    Console.WriteLine("Intente nuevamente...");
+    input = Console.ReadLine(); 
+    exitosaConv = int.TryParse(input, out numero);
+}
+
+cantAlumn = numero; 
+
 Console.WriteLine("Ingrese la cantidad de computadoras: ");
-string? recibido = Console.ReadLine();
-Console.WriteLine("Ingrese la cantidad de Alumnos: ");
-Console.ReadLine();
-if (int.TryParse(recibido, out int num))
+input = Console.ReadLine();
+exitosaConv = int.TryParse(input, out numero);
+
+
+while (exitosaConv == false || numero < 0)  // <- Mientras conversión exitosa sea falso o numero menor a 0 hacer:
 {
-    cantComputadoras = num;
+    Console.WriteLine("Intente nuevamente...");
+    input = Console.ReadLine();
+    exitosaConv = int.TryParse(input, out numero);
 }
+cantComps = numero;
+
+Console.WriteLine(cantAlumn);
+Console.WriteLine(cantComps);
+
+
+double promedio = (double)cantComps/ cantAlumn;
+Console.WriteLine($"Promedio : {promedio:F3}");
+
+if (cantAlumn <= cantComps)
+{
+    Console.WriteLine("Equipos suficientes");
+}
+
 else
 {
-    Console.WriteLine("ERROR");
+    Console.WriteLine("Faltan equipos");
 }
-if (cantAlumnos > cantComputadoras) 
-{ 
-    Console.WriteLine("Faltan computadoras para la sala de alumnos."); 
-}
-else
-    {
-    Console.WriteLine("Hay suficientes computadoras en la sala");
-    }
